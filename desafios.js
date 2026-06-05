1e7176bcc9c1c4bdc5153b47f13716041e86d15a
Dev-Kaique <167023631+DevKaiPereira@users.noreply.github.com>
2026-06-05 00:29:32 +0000

    Add initial implementation of createHelloWorld function and HTML file

---
+++ desafios.js
@@ -0,0 +1,14 @@
+/*DESAFIO 01*
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
